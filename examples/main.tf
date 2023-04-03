bd20f21b12c705e46d2869e09bc96ead19240b3d
Roman Timofeev <romantimofeev@yandex-team.ru>
2023-04-03 10:22:25 +0300

    fixs in examples with var type

--- examples/main.tf
+++ examples/main.tf
@@ -1,34 +1,34 @@
 module "net" {
-  source                       = "../"
+  source              = "../"
   labels              = { tag = "example" }
   network_description = "terraform-created"
   network_name        = "net-module-example"
-  create_vpc = true
+  create_vpc          = true
   public_subnets = [
     {
-      "v4_cidr_blocks" : "10.121.0.0/16",
+      "v4_cidr_blocks" : ["10.121.0.0/16"],
       "zone" : "ru-central1-a"
     },
     {
-      "v4_cidr_blocks" : "10.131.0.0/16",
+      "v4_cidr_blocks" : ["10.131.0.0/16"],
       "zone" : "ru-central1-b"
     },
     {
-      "v4_cidr_blocks" : "10.141.0.0/16",
+      "v4_cidr_blocks" : ["10.141.0.0/16"],
       "zone" : "ru-central1-c"
     },
   ]
   private_subnets = [
     {
-      "v4_cidr_blocks" : "10.221.0.0/16",
+      "v4_cidr_blocks" : ["10.221.0.0/16"],
       "zone" : "ru-central1-a"
     },
     {
-      "v4_cidr_blocks" : "10.231.0.0/16",
+      "v4_cidr_blocks" : ["10.231.0.0/16"],
       "zone" : "ru-central1-b"
     },
     {
-      "v4_cidr_blocks" : "10.241.0.0/16",
+      "v4_cidr_blocks" : ["10.241.0.0/16"],
       "zone" : "ru-central1-c"
     },
   ]
@@ -37,11 +37,11 @@ module "net" {
       destination_prefix : "172.16.0.0/16",
       next_hop_address : "10.131.0.10"
     },
-    ]
+  ]
   routes_private_subnets = [
     {
       destination_prefix : "172.16.0.0/16",
       next_hop_address : "10.231.0.10"
     },
-    ]
+  ]
 }
